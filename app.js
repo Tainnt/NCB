7056416f1681ce0058c729b6c3928573f39452ed
Tai Nguyen <tainguyen2912@gmail.com>
2018-10-23 11:52:07 +0700

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, resp){
   // Print the name of the file for which request is made.
-  fs.readFile("11.html",function(error, data){
+  fs.readFile("index.jade",function(error, data){
     if (error) {
       resp.writeHead(404);
       resp.write('Contents you are looking for-not found');
