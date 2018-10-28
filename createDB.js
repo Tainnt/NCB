3d0f9caae983453378fe5ce676a9cbb9e8e121de
Tai Nguyen <tainguyen2912@gmail.com>
2018-10-28 23:04:00 +0700

    upload code create DB

---
+++ createDB.js
@@ -0,0 +1,35 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: 'mysql-ncb.cdmcpyfgppxt.us-east-1.rds.amazonaws.com',
+    user: 'root',
+    password: '12345678',
+    database: 'mysql-ncb',
+});
+//connect.
+// conn.connect(function (err){
+//     //nếu có nỗi thì in ra
+//     if (err) throw err.stack;
+//     //nếu thành công
+//     var sql = "CREATE TABLE player (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT , username varchar(30) not null, password varchar(10) not null)";
+//     conn.query(sql, function (err) {
+//         if (err) throw err;
+//         console.log('Create table successful');
+//     });
+// });
+//close connect
+// conn.end(function (err) {
+//     if (err) throw err;
+//     console.log('DB closes');
+// });
+//insert
+// conn.connect(function (err){
+//     //nếu có nỗi thì in ra
+//     if (err) throw err.stack;
+//     //nếu thành công
+//     var sql = "INSERT INTO player(username,password) values ('Tai', '123'),('Pha','456'), ('Men', '789')";
+//     conn.query(sql, function (err) {
+//         if (err) throw err;
+//         console.log('Insert successful');
+//     });
+// });
--- script.js
+++
@@ -1,8 +0,0 @@
-function PrintName() {
-	Name =document.getElementById('txtTen').value;
-	Pass =document.getElementById('txtMatKhau').value;
-	if(Name != '' && Pass != '')
-		document.getElementById("hello").innerHTML = ('Chào bạn <b><i>'+Name+'<i><b>');
-	else
-		document.getElementById("hello").innerHTML = ('<b>Bạn chưa nhập tên người dùng hoặc mật khẩu<b>');
-}
