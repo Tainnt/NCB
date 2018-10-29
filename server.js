076db4ac589dd2d3b8996afe4b554a647cc31f26
Tai Nguyen <tainguyen2912@gmail.com>
2018-10-29 22:48:13 +0700

    test

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ http.createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello World - Do an nhung can ban");
 
-}).listen(8080);
+}).listen(process.env.PORT || 3000);
