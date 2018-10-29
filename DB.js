ab4f42dca19a3f448b9903fcdc442fe921c3274f
Tai Nguyen <tainguyen2912@gmail.com>
2018-10-29 14:11:58 +0700

    test

--- DB.js
+++ DB.js
@@ -7,13 +7,24 @@ var conn = mysql.createConnection({
     database: 'battle_ship',
 });
 
+// conn.connect(function(err) {
+//     //nếu có nỗi thì in ra
+//     if (err) throw err.stack;
+//     //nếu thành công
+//     var sql = "CREATE TABLE player (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT , username varchar(20) not null, password varchar(10) not null)";
+//     conn.query(sql, function(err) {
+//         if (err) throw err;
+//         console.log('Create table successful');
+//     });
+// });
+
 conn.connect(function(err) {
     //nếu có nỗi thì in ra
     if (err) throw err.stack;
     //nếu thành công
-    var sql = "CREATE TABLE player (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT , username varchar(20) not null, password varchar(10) not null)";
+    var sql = "INSERT INTO player(username,password) values ('Tai', '123'),('Pha','456'), ('Men', '789')";
     conn.query(sql, function(err) {
         if (err) throw err;
-        console.log('Create table successful');
+        console.log('Insert successful');
     });
 });
