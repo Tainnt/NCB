b3161181526b3b380120a4e6c8c07373b5083ac9
Tai Nguyen <tainguyen2912@gmail.com>
2018-11-06 00:34:42 +0700

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 server.listen(3000, function() {
